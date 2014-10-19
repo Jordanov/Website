8c4953a6f50221df63f05482fce446597f56fffe
Angel <angel_2002g@abv.bg>
2014-10-19 19:53:15 +0300

    wip angel.php

---
+++ angel.php
@@ -0,0 +1,35 @@
+<?php
+
+// заглавието на страницата
+$title = "Angel Zhekov";
+
+// извикване хедъра
+require_once("header.php");
+
+?>
+
+		<!-- Заглавие -->
+		<header>
+			<h1>Профил на агент: Angel Zhekov</h1>
+		</header>
+
+		<!-- Основно съдържание на страницата -->
+		<div id="content">
+
+			<div class="profile">
+				<img src="img/team/angel-crop.jpg" alt="angel">
+				<a style="width:575px;margin-bottom:20px" href="https://softuni.bg/users/profile/show/itaddams">Angel Zhekov</a>
+				<p>Агент ....</p>
+
+			</div>
+
+			<div class="clear"></div>
+
+		</div>
+
+<?php
+
+// извикваме футъра
+require_once("footer.php");
+
+?>
