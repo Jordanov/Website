1cd497cd923f5e5ed9bfd213b03a46f58ddd93e8
tsvetanov <tsvetanov.george@gmail.com>
2014-10-19 21:51:32 +0300

    wip tsvetanov.php

---
+++ tsvetanov.php
@@ -0,0 +1,35 @@
+<?php
+
+// заглавието на страницата
+$title = "Георги Цветанов";
+
+// извикване хедъра
+require_once("header.php");
+
+?>
+
+		<!-- Заглавие -->
+		<header>
+			<h1>Профил на агент: Георги Цветанов</h1>
+		</header>
+
+		<!-- Основно съдържание на страницата -->
+		<div id="content">
+
+			<div class="profile">
+				<img src="img/team/tsvetanov.jpg" alt="tsvetanov">
+				<a style="width:575px;margin-bottom:20px" href="https://softuni.bg/users/profile/show/jorkata06">Martin Varbanov</a>
+				<p>Агент ....</p>
+
+			</div>
+
+			<div class="clear"></div>
+
+		</div>
+
+<?php
+
+// извикваме футъра
+require_once("footer.php");
+
+?>
