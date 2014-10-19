1c255025e9a1cd05f3a5b1a490ac04ea86150d84
iliancho99 <iliqn.kordev@gmail.com>
2014-10-19 20:03:10 +0300

    wip iliyan.php

---
+++ iliyan.php
@@ -0,0 +1,35 @@
+<?php
+
+// заглавието на страницата
+$title = "Iliyan Kordev";
+
+// извикване хедъра
+require_once("header.php");
+
+?>
+
+		<!-- Заглавие -->
+		<header>
+			<h1>Профил на агент: Iliyan Kordev</h1>
+		</header>
+
+		<!-- Основно съдържание на страницата -->
+		<div id="content">
+
+			<div class="profile">
+				<img src="img/team/iliyan.jpg" alt="angel">
+				<a style="width:575px;margin-bottom:20px" href="https://softuni.bg/users/profile/show/iliyankordev">Iliyan Kordev</a>
+				<p>Агент ....</p>
+
+			</div>
+
+			<div class="clear"></div>
+
+		</div>
+
+<?php
+
+// извикваме футъра
+require_once("footer.php");
+
+?>
