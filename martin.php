bb87f4e21c0e0eac65ef2d78d1f3aecf7e4bb3c0
martin-varbanov96 <martin_varbanov96@abv.bg>
2014-10-19 20:08:02 +0300

    wip martin.php

---
+++ martin.php
@@ -0,0 +1,35 @@
+<?php
+
+// заглавието на страницата
+$title = "Martin Varbanov";
+
+// извикване хедъра
+require_once("header.php");
+
+?>
+
+		<!-- Заглавие -->
+		<header>
+			<h1>Профил на агент: Martin Varbanov</h1>
+		</header>
+
+		<!-- Основно съдържание на страницата -->
+		<div id="content">
+
+			<div class="profile">
+				<img src="img/team/martin-crop.jpg" alt="martin">
+				<a style="width:575px;margin-bottom:20px" href="https://softuni.bg/users/profile/show/martin_varbanov">Martin Varbanov</a>
+				<p>Агент ....</p>
+
+			</div>
+
+			<div class="clear"></div>
+
+		</div>
+
+<?php
+
+// извикваме футъра
+require_once("footer.php");
+
+?>
