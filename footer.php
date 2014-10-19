cdc2f4aa44a0c72a695373815e9bf1876137bc25
Roumen Damianoff <roumen@dawebs.com>
2014-10-19 19:22:50 +0300

    fixes cloudflare caching problem

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 
 		<!-- JS scripts -->
 		<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
-		<script type="text/javascript" src="js/default.js"></script>
+		<script type="text/javascript" src="js/default.js?"></script>
 	</body>
 
 </html>
