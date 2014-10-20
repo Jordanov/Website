48220f06a3d2dc175d8d2ce0ffeed1ba6ec04356
Roumen Damianoff <roumen@dawebs.com>
2014-10-20 15:22:27 +0300

    added piwik tracking

--- js/default.js
+++ js/default.js
@@ -7,4 +7,18 @@ $(document).ready(function()
 
 	$("#html_validator").attr('href', htmlUrl);
 	$("#css_validator").attr('href', cssUrl);
+
+	// piwik stats
+	var _paq = _paq || [];
+	_paq.push(['trackPageView']);
+	_paq.push(['enableLinkTracking']);
+
+	(function()
+	{
+		var u="//stats.dawebs.com/";
+		_paq.push(['setTrackerUrl', u+'piwik.php']);
+		_paq.push(['setSiteId', 13]);
+		var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+		g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+	})();
 });
